275568143397c735e971f40b7f08c57de50c4149
Thyago <t.y_josef7@hotmail.com>
2021-05-06 01:20:05 -0300

    configurando a view model

---
+++ ApCatalogoDeJogos/InputModel/JogoInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApCatalogoDeJogos.InputModel
+{
+    public class Class
+    {
+    }
+}
